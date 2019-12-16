9cf58796cd9ce35b9939eaf4b11c108b9414bacd
DevPYadav <devpyadav@gmail.com>
2019-12-16 16:36:36 +0530

    Updated testscrpt.sh on 16th Dec

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "this scipt is from github"
 NUMBER=$(($NUMBERONE+$NUMBERTWO))
 echo "$NUMBER"
 echo "this is completed by Dev P. Yadav"
-echo "this was run automatically always 12345"
+echo "this was run automatically always 123456"
